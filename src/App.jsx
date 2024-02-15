0b22bec596cb8ca1442511b158b71e0eb35edd09
Elyticus <125762622+Elyticus@users.noreply.github.com>
2024-02-15 20:54:36 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/header/Header";
+
+function App() {
+  return (
+    <>
+      <Header
+        pattern="/images/pattern-divider-desktop.svg"
+        dice="/images/icon-dice.svg"
+      />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+/* eslint-disable react/prop-types */
+export default function Header(props) {
+  const [adviceData, setAdviceData] = useState(
+    "Here you will get your daily advice"
+  );
+  const [adviceId, setAdviceId] = useState("");
+
+  function handleChangeButton() {
+    fetch("https://api.adviceslip.com/advice")
+      .then((res) => res.json())
+      .then((data) => {
+        setAdviceData(data.slip.advice);
+        setAdviceId(data.slip.id);
+      });
+  }
+
+  return (
+    <header>
+      <p className="header-title">
+        Advice # <span id="advice-id">{adviceId}</span>
+      </p>
+      <p className="advice-text">{adviceData}</p>
+      <img
+        className="pattern-divider"
+        src={props.pattern}
+        alt="Pattern Divider Desktop"
+      />
+      <button onClick={handleChangeButton} className="dice">
+        <img src={props.dice} alt="Dice image" />
+      </button>
+    </header>
+  );
+}
