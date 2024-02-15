ee19e4f537ebb9810aeaa58567f46b88288ef206
Elyticus <125762622+Elyticus@users.noreply.github.com>
2024-02-16 00:26:39 +0200

    dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
+import { useState } from "react";
 import Header from "./components/header/Header";
 
 function App() {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+    const modesElements = document.getElementsByClassName("modes");
+    if (!isChecked) {
+      document.body.classList.add("dark-class");
+      for (let i = 0; i < modesElements.length; i++) {
+        modesElements[i].classList.add("dark-title");
+      }
+    } else {
+      document.body.classList.remove("dark-class");
+      for (let i = 0; i < modesElements.length; i++) {
+        modesElements[i].classList.remove("dark-title");
+      }
+    }
+  };
+
   return (
-    <>
+    <div className="toggle-mode">
+      <p className="modes">Switch Modes</p>
+      <label className="switch">
+        <input type="checkbox" onChange={handleCheckboxChange} />
+        <span className="slider round"></span>
+      </label>
+
       <Header
+        isChecked={isChecked}
         pattern="/images/pattern-divider-desktop.svg"
         dice="/images/icon-dice.svg"
       />
-    </>
+    </div>
   );
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,6 +6,19 @@ export default function Header(props) {
     "It's easy to sit up and take notice, what's difficult is getting up and taking action."
   );
   const [adviceId, setAdviceId] = useState("117");
+  const [addMode, setAddMode] = useState(false);
+
+  const toggleModes = () => {
+    setAddMode(!addMode);
+    const headerElement = document.querySelector("header");
+    if (headerElement) {
+      if (addMode) {
+        headerElement.classList.remove("dark-header");
+      } else {
+        headerElement.classList.add("dark-header");
+      }
+    }
+  };
 
   function handleChangeButton() {
     fetch("https://api.adviceslip.com/advice")
@@ -17,11 +30,16 @@ export default function Header(props) {
   }
 
   return (
-    <header>
+    <header
+      className={props.isChecked ? "header-class" : ""}
+      onClick={toggleModes}
+    >
       <p className="header-title">
         Advice # <span id="advice-id">{adviceId}</span>
       </p>
-      <p className="advice-text">&quot;{adviceData}&quot;</p>
+      <p className={props.isChecked ? "dark-advice-text" : "advice-text"}>
+        &quot;{adviceData}&quot;
+      </p>
       <img
         className="pattern-divider"
         src={props.pattern}
